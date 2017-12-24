faab63b2f91b53982eca0b9412849798a27daa84
unknown <Chad@laptop-chad.techromancer.local>
2017-12-24 15:12:19 -0700

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'james fag',
-    text: 'Hey, I may be gay.',
-    createAt: 164
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('createdMessage', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('createdMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
